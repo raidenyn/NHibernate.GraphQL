a58dbaa03f1216b680ebdac248e668416f31c185
Yuriy Nagaev <yuriy@nagaev.me>
2019-01-15 22:55:43 +0700

    Added not tested bulk select requests

---
+++ src/NHibernate.GraphQL/BulkSelectExtension.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NHibernate.Linq;
+
+namespace NHibernate.GraphQL
+{
+    /// <summary>
+    /// Provides splitting database requests to limit sql request body size
+    /// </summary>
+    public static class BulkQueryExtension
+    {
+        private const int DefaultBatchSize = 1000;
+
+        /// <summary>
+        /// Synchroniosly requests data in databse
+        /// </summary>
+        /// <typeparam name="TDbObject">Database mapped object type</typeparam>
+        /// <typeparam name="TResult">Result object type</typeparam>
+        /// <typeparam name="TId">Parameter object type</typeparam>
+        /// <param name="query">Original database LINQ query</param>
+        /// <param name="select">Function to difine filteration by the ids and selection result objects</param>
+        /// <param name="ids">Collection of the id objects</param>
+        /// <param name="batchSize">Maximum count of ids in one select</param>
+        /// <returns></returns>
+        public static IEnumerable<TResult> BulkSelect<TDbObject, TResult, TId>(
+            this IQueryable<TDbObject> query,
+            Func<IQueryable<TDbObject>, IReadOnlyCollection<TId>, IQueryable<TResult>> select,
+            IReadOnlyCollection<TId> ids,
+            int batchSize = DefaultBatchSize)
+        {
+            return BulkSelectAsync(query, select, ids, batchSize, batch => Task.FromResult(batch.ToList())).Result;
+        }
+
+        /// <summary>
+        /// Asynchroniosly requests data in databse
+        /// </summary>
+        /// <typeparam name="TDbObject">Database mapped object type</typeparam>
+        /// <typeparam name="TResult">Result object type</typeparam>
+        /// <typeparam name="TId">Parameter object type</typeparam>
+        /// <param name="query">Original database LINQ query</param>
+        /// <param name="select">Function to difine filteration by the ids and selection result objects</param>
+        /// <param name="ids">Collection of the id objects</param>
+        /// <param name="batchSize">Maximum count of ids in one select</param>
+        /// <param name="cancellationToken">Token to cancel the requests</param>
+        /// <returns></returns>
+        public static Task<IEnumerable<TResult>> BulkSelectAsync<TDbObject, TResult, TId>(
+            this IQueryable<TDbObject> query,
+            Func<IQueryable<TDbObject>, IReadOnlyCollection<TId>, IQueryable<TResult>> select,
+            IReadOnlyCollection<TId> ids,
+            int batchSize = DefaultBatchSize,
+            CancellationToken cancellationToken = default)
+        {
+            return BulkSelectAsync(query, select, ids, batchSize, batch => batch.ToListAsync(cancellationToken));
+        }
+
+        /// <summary>
+        /// Asynchroniosly requests data in databse
+        /// </summary>
+        /// <typeparam name="TDbObject">Database mapped object type</typeparam>
+        /// <typeparam name="TResult">Result object type</typeparam>
+        /// <typeparam name="TId">Parameter object type</typeparam>
+        /// <param name="query">Original database LINQ query</param>
+        /// <param name="select">Function to difine filteration by the ids and selection result objects</param>
+        /// <param name="ids">Collection of the id objects</param>
+        /// <param name="cancellationToken">Token to cancel the requests</param>
+        /// <returns></returns>
+        public static Task<IEnumerable<TResult>> BulkSelectAsync<TDbObject, TResult, TId>(
+            this IQueryable<TDbObject> query,
+            Func<IQueryable<TDbObject>, IReadOnlyCollection<TId>, IQueryable<TResult>> select,
+            IReadOnlyCollection<TId> ids,
+            CancellationToken cancellationToken = default)
+        {
+            return BulkSelectAsync(query, select, ids, DefaultBatchSize, cancellationToken);
+        }
+
+        /// <summary>
+        /// Synchroniosly requests data in databse
+        /// </summary>
+        /// <typeparam name="TDbObject">Database mapped object type</typeparam>
+        /// <typeparam name="TResult">Result object type</typeparam>
+        /// <typeparam name="TId">Parameter object type</typeparam>
+        /// <param name="query">Original database LINQ query</param>
+        /// <param name="select">Function to difine filteration by the ids and selection result objects</param>
+        /// <param name="getId">Function to return id from result object</param>
+        /// <param name="ids">Collection of the id objects</param>
+        /// <param name="batchSize">Maximum count of ids in one select</param>
+        /// <returns></returns>
+        public static IDictionary<TId, TResult> BulkSelect<TDbObject, TResult, TId>(
+            this IQueryable<TDbObject> query,
+            Func<IQueryable<TDbObject>, IReadOnlyCollection<TId>, IQueryable<TResult>> select,
+            Func<TResult, TId> getId,
+            IReadOnlyCollection<TId> ids,
+            int batchSize = DefaultBatchSize)
+        {
+            return BulkSelect(query, select, ids, batchSize).ToDictionary(item => getId(item), item => item);
+        }
+
+        /// <summary>
+        /// Asynchroniosly requests data in databse
+        /// </summary>
+        /// <typeparam name="TDbObject">Database mapped object type</typeparam>
+        /// <typeparam name="TResult">Result object type</typeparam>
+        /// <typeparam name="TId">Parameter object type</typeparam>
+        /// <param name="query">Original database LINQ query</param>
+        /// <param name="select">Function to difine filteration by the ids and selection result objects</param>
+        /// <param name="getId">Function to return id from result object</param>
+        /// <param name="ids">Collection of the id objects</param>
+        /// <param name="batchSize">Maximum count of ids in one select</param>
+        /// <param name="cancellationToken">Token to cancel the requests</param>
+        /// <returns></returns>
+        public static async Task<IDictionary<TId, TResult>> BulkSelectAsync<TDbObject, TResult, TId>(
+            this IQueryable<TDbObject> query,
+            Func<IQueryable<TDbObject>, IReadOnlyCollection<TId>, IQueryable<TResult>> select,
+            Func<TResult, TId> getId,
+            IReadOnlyCollection<TId> ids,
+            int batchSize = DefaultBatchSize,
+            CancellationToken cancellationToken = default)
+        {
+            var results = await BulkSelectAsync(query, select, ids, batchSize, batch => batch.ToListAsync(cancellationToken)).ConfigureAwait(false);
+
+            return results.ToDictionary(item => getId(item), item => item);
+        }
+
+        /// <summary>
+        /// Asynchroniosly requests data in databse
+        /// </summary>
+        /// <typeparam name="TDbObject">Database mapped object type</typeparam>
+        /// <typeparam name="TResult">Result object type</typeparam>
+        /// <typeparam name="TId">Parameter object type</typeparam>
+        /// <param name="query">Original database LINQ query</param>
+        /// <param name="select">Function to difine filteration by the ids and selection result objects</param>
+        /// <param name="getId">Function to return id from result object</param>
+        /// <param name="ids">Collection of the id objects</param>
+        /// <param name="cancellationToken">Token to cancel the requests</param>
+        /// <returns></returns>
+        public static Task<IDictionary<TId, TResult>> BulkSelectAsync<TDbObject, TResult, TId>(
+            this IQueryable<TDbObject> query,
+            Func<IQueryable<TDbObject>, IReadOnlyCollection<TId>, IQueryable<TResult>> select,
+            Func<TResult, TId> getId,
+            IReadOnlyCollection<TId> ids,
+            CancellationToken cancellationToken = default)
+        {
+            return BulkSelectAsync(query, select, getId, ids, DefaultBatchSize, cancellationToken);
+        }
+
+        private static async Task<IEnumerable<TResult>> BulkSelectAsync<TDbObject, TResult, TId>(
+            this IQueryable<TDbObject> query,
+            Func<IQueryable<TDbObject>, IReadOnlyCollection<TId>, IQueryable<TResult>> select,
+            IReadOnlyCollection<TId> ids,
+            int batchSize,
+            Func<IQueryable<TResult>, Task<List<TResult>>> execute)
+        {
+            var results = new List<List<TResult>>(capacity: ids.Count / batchSize + 1);
+
+            for (int offset = 0; offset < ids.Count; offset += batchSize)
+            {
+                var idsBatch = ids.Skip(offset).Take(batchSize).ToArray();
+
+                var batchQuery = select(query, idsBatch);
+
+                results.Add(await batchQuery.ToListAsync().ConfigureAwait(false));
+            }
+
+            return results.SelectMany(result => result);
+        }
+    }
+}
---
+++ src/NHibernate.GraphQL/BulkSelectManyExtension.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NHibernate.Linq;
+
+namespace NHibernate.GraphQL
+{
+    /// <summary>
+    /// Provides splitting database requests to limit sql request body size
+    /// </summary>
+    public static class BulkSelectManyExtension
+    {
+        private const int DefaultBatchSize = 500;
+
+        /// <summary>
+        /// Bulk select from database for junced objects
+        /// </summary>
+        /// <typeparam name="TDbObject">Database mapped object type</typeparam>
+        /// <typeparam name="TResult">Result object type</typeparam>
+        /// <typeparam name="TJunctionId">Junced id type</typeparam>
+        /// <typeparam name="TResultId">Result id type</typeparam>
+        /// <param name="query">Original database LINQ query</param>
+        /// <param name="filtering">Function for creating filtered query</param>
+        /// <param name="selectResult">Select result statement</param>
+        /// <param name="selectJunction">Select junction statement</param>
+        /// <param name="getId">Returns result id from result object</param>
+        /// <param name="ids">List of requested ids</param>
+        /// <param name="batchSize">Maximum cid count in one select batch</param>
+        /// <returns>List of result objects mapped to id</returns>
+        public static ILookup<TJunctionId, TResult> BulkSelectMany<TDbObject, TResult, TJunctionId, TResultId>(
+            this IQueryable<TDbObject> query,
+            Func<IQueryable<TDbObject>, IReadOnlyCollection<TJunctionId>, IQueryable<TDbObject>> filtering,
+            Func<IQueryable<TDbObject>, IQueryable<TResult>> selectResult,
+            Func<IQueryable<TDbObject>, IQueryable<Junction<TResultId, TJunctionId>>> selectJunction,
+            Func<TResult, TResultId> getId,
+            IReadOnlyCollection<TJunctionId> ids,
+            int batchSize = DefaultBatchSize)
+        {
+            Task<(IEnumerable<TResult>, IEnumerable<Junction<TResultId, TJunctionId>>)> Executor(
+                IFutureEnumerable<TResult> resultQuery,
+                IFutureEnumerable<Junction<TResultId, TJunctionId>> junctionQuery)
+            {
+                return Task.FromResult((resultQuery.GetEnumerable(), junctionQuery.GetEnumerable()));
+            }
+
+            return BulkSelectManyAsync(query, filtering, selectResult, selectJunction, getId, ids, DefaultBatchSize, Executor).Result;
+        }
+
+        /// <summary>
+        /// Bulk select from database for junced objects
+        /// </summary>
+        /// <typeparam name="TDbObject">Database mapped object type</typeparam>
+        /// <typeparam name="TResult">Result object type</typeparam>
+        /// <typeparam name="TJunctionId">Junced id type</typeparam>
+        /// <typeparam name="TResultId">Result id type</typeparam>
+        /// <param name="query">Original database LINQ query</param>
+        /// <param name="filtering">Function for creating filtered query</param>
+        /// <param name="selectResult">Select result statement</param>
+        /// <param name="selectJunction">Select junction statement</param>
+        /// <param name="getId">Returns result id from result object</param>
+        /// <param name="ids">List of requested ids</param>
+        /// <param name="batchSize">Maximum cid count in one select batch</param>
+        /// <param name="cancellationToken">Cancellation tocken for sql requests</param>
+        /// <returns>List of result objects mapped to id</returns>
+        public static Task<ILookup<TJunctionId, TResult>> BulkSelectManyAsync<TDbObject, TResult, TJunctionId, TResultId>(
+            this IQueryable<TDbObject> query,
+            Func<IQueryable<TDbObject>, IReadOnlyCollection<TJunctionId>, IQueryable<TDbObject>> filtering,
+            Func<IQueryable<TDbObject>, IQueryable<TResult>> selectResult,
+            Func<IQueryable<TDbObject>, IQueryable<Junction<TResultId, TJunctionId>>> selectJunction,
+            Func<TResult, TResultId> getId,
+            IReadOnlyCollection<TJunctionId> ids,
+            int batchSize = DefaultBatchSize,
+            CancellationToken cancellationToken = default)
+        {
+            async Task<(IEnumerable<TResult>, IEnumerable<Junction<TResultId, TJunctionId>>)> ExecutorAsync(
+                IFutureEnumerable<TResult> resultQuery,
+                IFutureEnumerable<Junction<TResultId, TJunctionId>> junctionQuery)
+            {
+                return (await resultQuery.GetEnumerableAsync().ConfigureAwait(false),
+                        await junctionQuery.GetEnumerableAsync().ConfigureAwait(false));
+            }
+
+            return BulkSelectManyAsync(query, filtering, selectResult, selectJunction, getId, ids, DefaultBatchSize, ExecutorAsync);
+        }
+
+        /// <summary>
+        /// Bulk select from database for junced objects
+        /// </summary>
+        /// <typeparam name="TDbObject">Database mapped object type</typeparam>
+        /// <typeparam name="TResult">Result object type</typeparam>
+        /// <typeparam name="TJunctionId">Junced id type</typeparam>
+        /// <typeparam name="TResultId">Result id type</typeparam>
+        /// <param name="query">Original database LINQ query</param>
+        /// <param name="filtering">Function for creating filtered query</param>
+        /// <param name="selectResult">Select result statement</param>
+        /// <param name="selectJunction">Select junction statement</param>
+        /// <param name="getId">Returns result id from result object</param>
+        /// <param name="ids">List of requested ids</param>
+        /// <param name="cancellationToken">Cancellation tocken for sql requests</param>
+        /// <returns>List of result objects mapped to id</returns>
+        public static Task<ILookup<TJunctionId, TResult>> BulkSelectManyAsync<TDbObject, TResult, TJunctionId, TResultId>(
+            this IQueryable<TDbObject> query,
+            Func<IQueryable<TDbObject>, IReadOnlyCollection<TJunctionId>, IQueryable<TDbObject>> filtering,
+            Func<IQueryable<TDbObject>, IQueryable<TResult>> selectResult,
+            Func<IQueryable<TDbObject>, IQueryable<Junction<TResultId, TJunctionId>>> selectJunction,
+            Func<TResult, TResultId> getId,
+            IReadOnlyCollection<TJunctionId> ids,
+            CancellationToken cancellationToken = default)
+        {
+            return BulkSelectManyAsync(query, filtering, selectResult, selectJunction, getId, ids, DefaultBatchSize, cancellationToken);
+        }
+
+        private static async Task<ILookup<TJunctionId, TResult>> BulkSelectManyAsync<TDbObject, TResult, TJunctionId, TResultId>(
+            this IQueryable<TDbObject> query,
+            Func<IQueryable<TDbObject>, IReadOnlyCollection<TJunctionId>, IQueryable<TDbObject>> filtering,
+            Func<IQueryable<TDbObject>, IQueryable<TResult>> selectResult,
+            Func<IQueryable<TDbObject>, IQueryable<Junction<TResultId, TJunctionId>>> selectJunction,
+            Func<TResult, TResultId> id,
+            IReadOnlyCollection<TJunctionId> values,
+            int batchSize,
+            Func<IFutureEnumerable<TResult>, IFutureEnumerable<Junction<TResultId, TJunctionId>>, Task<(IEnumerable<TResult>, IEnumerable<Junction<TResultId, TJunctionId>>)>> executor)
+        {
+            var results = new List<IEnumerable<TResult>>(capacity: values.Count / batchSize + 1);
+            var junctions = new List<IEnumerable<Junction<TResultId, TJunctionId>>>(capacity: results.Count);
+
+            for (int offset = 0; offset < results.Count; offset += batchSize)
+            {
+                TJunctionId[] batchValues = values.Skip(offset).Take(batchSize).ToArray();
+
+                IQueryable<TDbObject> batchQuery = filtering(query, batchValues);
+
+                IFutureEnumerable<TResult> resultQuery = selectResult(batchQuery).Distinct().ToFuture();
+                IFutureEnumerable<Junction<TResultId, TJunctionId>> junctionQuery = selectJunction(batchQuery).Distinct().ToFuture();
+
+                var (result, junction) = await executor(resultQuery, junctionQuery).ConfigureAwait(false);
+
+                results.Add(result);
+                junctions.Add(junction);
+            }
+
+            Dictionary<TResultId, TResult> dic = results.SelectMany(result => result).ToDictionary(id);
+
+            return junctions
+                .SelectMany(junction => junction)
+                .Select(item =>
+                {
+                    if (dic.TryGetValue(item.ResultId, out var result))
+                    {
+                        return new { item.JunctionId, Result = result };
+                    }
+                    return null;
+                })
+                .Where(item => item != null)
+                .ToLookup(item => item.JunctionId, item => item.Result);
+        }
+    }
+}
